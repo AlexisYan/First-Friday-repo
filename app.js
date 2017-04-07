5a771687b974f58b11d4b08e0300f64d9c2559f5
Alexis Yan <yanranalexis@gmail.com>
2017-04-07 14:38:41 -0700

    third question

--- app.js
+++ app.js
@@ -47,9 +47,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a,b,c){ //eslint-disable-line
-
+var sumThreeNumber = a + b + c;
+var multiplyThreeNumber = a * b * c;
+var questionSumAndMultiply = [sumThreeNumber, multiplyThreeNumber,
+                               a + ' ' + 'and' + ' ' + b + ' ' + 'and' + ' ' + c + ' ' + 'sum to' + ' ' + sumThreeNumber + '.',
+                              'The product of' + ' ' + a + ' '+ 'and' + ' ' + b + ' ' + 'and' + ' ' + c + ' ' + 'is' + ' ' + multiplyThreeNumber + '.',];
+console.log (questionSumAndMultiply);
+return questionSumAndMultiply;
 }
-
+testSumAndMultiply(4, 7, 5);
 // Here is the test for sumAndMultiply(); uncomment it to run it
 // testSumAndMultiply(4,7,5);
 
